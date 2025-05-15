b785e59a4e7c877941cd60bdb133f05a9be26989
Дмитрий <dimabogolepov2002@mail.ru>
2025-05-15 15:59:13 +0300

    style: added adaptation styles for 325px resolution and fixed others. fix: fixed cart double adding products error.

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -25,7 +25,6 @@ export class ProductComponent implements OnInit {
   addProductToCart() {
     this.cartService.count++
     this.cartService.sum = this.cartService.sum + this.macaroon.price;
-    this.addToCartEvent.emit(this.macaroon.title);
   }
 
 }
